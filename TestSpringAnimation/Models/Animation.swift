202d6689fe46b35c84cda899b102cc77d88aaa94
BezborodovPavel <bezborodovp@gmail.com>
2022-05-10 22:13:56 +0500

    change to optimal code

--- TestSpringAnimation/Models/Animation.swift
+++ TestSpringAnimation/Models/Animation.swift
@@ -18,12 +18,10 @@ struct Animation {
     static func getRundomAnimations() -> [Animation] {
         
         var animations: [Animation] = []
-        let countCurve = Spring.AnimationCurve.allCases.count
         
         for typeAnimation in Spring.AnimationPreset.allCases {
             
-            let randomСurve = Int.random(in: 0..<countCurve)
-            let curve = Spring.AnimationCurve.allCases[randomСurve]
+            let curve = Spring.AnimationCurve.allCases.randomElement()
             let force = round(Double.random(in: 1...5) * 10) / 10
             let duration = round(Double.random(in: 0.3...1) * 10) / 10
             let delay = round(Double.random(in: 0...1) * 10) / 10
